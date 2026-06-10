96c1978f22adc989f640e51bbc8e737ddfb9cc3d
fatimah7643 <fatimah013@gmail.com>
2026-06-10 08:46:02 +0800

    complete app - sprint 1-8, deploy render

--- kasir_app/android/app/build.gradle.kts
+++ kasir_app/android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     defaultConfig {
         applicationId = "com.umkm.kasir_app"
         minSdk = 26
-        targetSdk = 29
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0.0"
     }
--- kasir_app/lib/core/constants/app_constants.dart
+++ kasir_app/lib/core/constants/app_constants.dart
@@ -1,7 +1,7 @@
 class AppConstants {
   // Ganti dengan IP lokal PC kamu
   // Cek dengan: ipconfig → IPv4 Address
-  static const String baseUrl = 'http://10.24.159.174:3000/api';
+  static const String baseUrl = 'https://kasir-backend.onrender.com/api';
   
   static const String tokenKey = 'access_token';
   static const String refreshTokenKey = 'refresh_token';
